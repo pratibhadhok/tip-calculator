52c449af1737f6492787677253f4f886139abab1
Pratibha Dhok <pratvish@unknown-10-73-222-x.yahoo.com>
2017-01-20 09:53:40 -0800

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by  Pratibha Dhok on 1/20/17.
+//  Copyright © 2017 Pratibha Dhok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by  Pratibha Dhok on 1/20/17.
+//  Copyright © 2017 Pratibha Dhok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
